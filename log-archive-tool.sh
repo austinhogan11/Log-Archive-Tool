268bb1b78e0d59336096a761935d51ecd28eb8d9
Austin Hogan <austinhogandev@gmail.com>
2025-03-08 10:34:06 -0500

    Add input validation for directory paths

--- log-archive-tool.sh
+++ log-archive-tool.sh
@@ -3,8 +3,20 @@ echo "Log Archive Tool Activated"
 # Directory of logs to be archived
 LOG_DIRECTORY=$1
 
+# Check if LOG_DIRECTORY exists and exit if it doesn't
+if [ ! -d "$LOG_DIRECTORY" ]; then
+  echo "Log directory does not exist."
+  exit 1
+fi
+
 # Directory where the compressed logs will be stored
 LOG_STORAGE_DIRECTORY=$2
+
+# Check if LOG_STORAGE_DIRECTORY exists and create it if it doesn't
+if [ ! -d "$LOG_STORAGE_DIRECTORY" ]; then
+  echo "Log storage directory does not exist. Creating it now."
+  mkdir -p $LOG_STORAGE_DIRECTORY
+fi
 
 # Check if the directories exist
 if [ ! -d "$LOG_DIRECTORY" ]; then
